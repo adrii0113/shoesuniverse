bf881eb9f0e750ad814468755bf7be8e5709656e
adrii0113 <adrian190401@gmail.com>
2023-04-12 18:01:41 +0200

    add index in map function moviecarrousel component x3

--- src/components/Populartvcarrousel.tsx
+++ src/components/Populartvcarrousel.tsx
@@ -88,8 +88,8 @@ export default function Populartvcarrousel () {
                         </ButtonBack>
                         <Slider className="carousel__sliderLarge">
                             {
-                                populartvshows.map((populartvshow) => (
-                                    <Slide className="carousel__inner-slideLarge" index={0}>
+                                populartvshows.map((populartvshow, index) => (
+                                    <Slide className="carousel__inner-slideLarge" index={0} key={index}>
                                 <motion.div className="gallery-cell w-full h-full" whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
                                     <div className="relative w-full h-full lg:block hidden">
                                     <img src={`https://image.tmdb.org/t/p/original${populartvshow.poster_path}`} alt="sitting area" className="object-center object-cover w-full h-full" />
--- src/components/elements/explore/Explore.tsx
+++ src/components/elements/explore/Explore.tsx
@@ -95,13 +95,13 @@ export default function Explore(){
             </div> */}
 
             {
-                movies.map((movieArray)=>(
+                movies.map((movieArray, index)=>(
                     
                     // movieArray.map((movie)=>(
 
 
 
-                        <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                        <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700" key={index}>
                     <a href="#">
                         <img className="p-8 rounded-t-lg" src="" alt="product image" />
                     </a>
