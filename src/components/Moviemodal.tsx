bf823669926b3b2c0879023475c4686a1f6a7209
adrii0113 <adrian190401@gmail.com>
2023-04-12 17:44:38 +0200

    resolve problmes while deploying

--- src/components/Moviemodal.tsx
+++ src/components/Moviemodal.tsx
@@ -37,9 +37,9 @@ export default function Moviemodal (){
             className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
           >
             <div className="relative w-auto my-6 mx-auto max-w-3xl">
-              {/*content*/}
+              
               <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-                {/*header*/}
+                
                 <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
                   <h3 className="text-3xl font-semibold">
                     Modal Title
@@ -58,8 +58,8 @@ export default function Moviemodal (){
                   <p className="my-4 text-slate-500 text-lg leading-relaxed">
                     I always felt like I could do anything. That’s the main
                     thing people are controlled by! Thoughts- their perception
-                    of themselves! They're slowed down by their perception of
-                    themselves. If you're taught you can’t do anything, you
+                    of themselves! Theyre slowed down by their perception of
+                    themselves. If youre taught you can’t do anything, you
                     won’t do anything. I was taught I could do everything.
                   </p>
                 </div>
--- src/pages/[id].tsx
+++ src/pages/[id].tsx
@@ -1,13 +1,12 @@
-import { useRouter } from "next/router";
+
 
 export default function post(props: string) {
 
-    const router = useRouter();
-    console.log(router,'routes')
+    
 
     return(
 
-        <h2>post {router.query.id}</h2>
+        <p>aa</p>
 
     )
 
