482f4214936f39ddb25989755a8154372fc5fa81
adrii0113 <adrian190401@gmail.com>
2023-04-12 18:40:04 +0200

    change moviecarrousel & populartvcarrousel components tsx to jsx extension

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,7 +1,32 @@
-export {};
+/// <reference types="node" />
 
-declare global {
-  interface Window {
-    somePropertyHere: any;
+declare module 'dir-obj' {
+  import { Stats } from "fs";
+
+  export interface readOptions {
+    filter?: RegExp | Filter,
+    dirTransform?: DirTransform,
+    fileTransform?: FileTransform
+  }
+
+  export type Filter = (file: File) => boolean;
+  export type DirTransform = (file: File, value: any) => any;
+  export type FileTransform = (file: File) => any;
+
+  export function readDirectory(dir: string, options?: readOptions): object;
+
+  export class File {
+    key: string;
+    readonly path: string;
+    readonly fullpath: string;
+    readonly ext: string;
+    readonly name: string;
+    readonly basename: string;
+
+    constructor(dir: string, file: string);
+
+    readonly attributes: Stats;
+    readonly isDirectory: boolean;
+    readonly isRequirable: boolean;
   }
 }
