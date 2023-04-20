fe4d38915fae4ec2d25158545c61b41f54482feb
adrii0113 <adrian190401@gmail.com>
2023-04-20 18:23:20 +0200

    add router link to topbar

--- src/components/Topbarmenu.jsx
+++ src/components/Topbarmenu.jsx
@@ -46,11 +46,19 @@ export default function Topbarmenu (){
                     </li>
 
                     <li>
-                    <button type="button" className="text-white bg-white-gold hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-white-gold dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Sign in</button>
+                    <button type="button" className="text-white bg-white-gold hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-white-gold dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+                        
+                        <Link href="/login">Log in</Link>
+                        
+                        </button>
                     </li>
 
                     <li>
-                    <button type="button" className="text-white bg-white-gold hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-white-gold dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" onClick={()=>initUserSession()}>Sign up</button>
+                    <button type="button" className="text-white bg-white-gold hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-white-gold dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                    //  onClick={()=>initUserSession()}
+                     >
+                        <Link href="/register">Sign up</Link>
+                     </button>
                     {/* <button onClick={()=>initUserSession()}>Sign up</button> */}
                     </li>
 
